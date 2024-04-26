151854b27670feaba12fb3f1e43ccba7eb1deb11
SAFA IDAM HAMED <safa.idamhamed2@gmail.com>
2024-04-26 17:55:58 +0100

    PFE prod :sparkles:

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -4,6 +4,12 @@ import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get(("/"), req, res => {
+  res.json({
+    message: "Welcome to the API"
+  })
+})
+
 router.get("/users", test)
 router.get("/username", username)
 router.get("/:id", user)
